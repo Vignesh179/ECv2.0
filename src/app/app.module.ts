0c1c9c161d153dfd744abd1733640fdd37f1a0d4
Pethuvignesh K <79147341+VignesCoOl@users.noreply.github.com>
2021-03-10 12:55:38 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ProductsComponent } from './products/products.component';
 import { AboutComponent } from './about/about.component';
-import {MatIconModule} from '@angular/material/icon';
-import {MatCardModule} from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MycartComponent } from './mycart/mycart.component';
-import {MatTabsModule} from '@angular/material/tabs';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatButtonModule} from '@angular/material/button';
-import {MatTableModule} from '@angular/material/table';
-import {MatGridListModule} from '@angular/material/grid-list';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { HomeComponent } from './home/home.component';
-
 
 
 @NgModule({
@@ -35,7 +34,7 @@ import { HomeComponent } from './home/home.component';
     MatToolbarModule,
     MatIconModule,
     MatCardModule,
-    FormsModule, ReactiveFormsModule,HttpClientModule, MatTabsModule, MatFormFieldModule, MatButtonModule, MatTableModule,MatGridListModule
+    FormsModule, ReactiveFormsModule, HttpClientModule, MatTabsModule, MatFormFieldModule, MatButtonModule, MatTableModule, MatGridListModule
 
   ],
   providers: [],
