2e776abab887cb57c61b520bda06b5680605d2d2
Pethuvignesh K <79147341+VignesCoOl@users.noreply.github.com>
2021-03-12 16:19:04 +0530

    Update products.component.ts

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -10,13 +10,13 @@ import { Component, OnInit } from '@angular/core';
 export class ProductsComponent implements OnInit {
 
   //variables
-  pname:any = '';
-  pprice:any; 
-  products:any = [];
-  tot:any;
-  imgprd:any;
-  inca:any;
-  fval:any
+  pname: any = '';
+  pprice: any;
+  products: any = [];
+  tot: any;
+  imgprd: any;
+  inca: any;
+  fval: any
   // deca:any =1;
 
 
@@ -27,53 +27,53 @@ export class ProductsComponent implements OnInit {
       this.products = JSON.parse(a);
     }
     let total = localStorage.getItem('tot');
-    if(total){
+    if (total) {
       this.tot = total;
     }
-   }
-   //add product function
+  }
+  //add product function
   addProduct() {
     let list = {
       name: this.pname,
       price: this.pprice,
       imgprd: this.imgprd,
-      inca:this.inca,
-      tot:this.tot
+      inca: this.inca,
+      tot: this.tot
       // dec:this.deca
     }
-    
+
     this.products.push(list);
     console.log(this.products);
     localStorage.setItem('products', JSON.stringify(this.products));
 
 
-    this.tot = this.products.reduce((a:any,b:any)=>{
+    this.tot = this.products.reduce((a: any, b: any) => {
       return a + parseInt(b.price) * (b.inca);
-    },0);
-    
-    localStorage.setItem('tot',this.tot);
+    }, 0);
+
+    localStorage.setItem('tot', this.tot);
   }
- 
+
   delete(i: any) {
     this.products.splice(i, 1);
   }
-  clear(){
-    let li={
-      name: this.pname="",
-      price: this.pprice="",
-      imgprd: this.imgprd="",
-      inca:this.inca="",
+  clear() {
+    let li = {
+      name: this.pname = "",
+      price: this.pprice = "",
+      imgprd: this.imgprd = "",
+      inca: this.inca = "",
     }
   }
-//   incF(){
-//     this.inc++
-//   }
-//   decF(){
-//     this.dec = this.inc--;
-//     return this.dec;
-// }
+  //   incF(){
+  //     this.inc++
+  //   }
+  //   decF(){
+  //     this.dec = this.inc--;
+  //     return this.dec;
+  // }
   ngOnInit(): void {
   }
-  
+
 
 }
